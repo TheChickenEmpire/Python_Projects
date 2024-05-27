e3b95d574ac80a768b6f83d9ec0d8929ef28b3e6
TheChickenEmpire <164987492+TheChickenEmpire@users.noreply.github.com>
2024-05-27 07:10:21 +0000

    hi

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from PIL import Image
 st.set_page_config(page_title='The_Chicken_Empire', page_icon=":chicken:", layout= 'wide')
-st.color_picker('white')
 teams,  = st.columns([1])
 with teams:
     st.image(Image.open("Stem.png"), width= 600)
