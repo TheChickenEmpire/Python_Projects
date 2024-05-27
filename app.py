3a300461e447138405333b013e7558a07302ed63
TheChickenEmpire <164987492+TheChickenEmpire@users.noreply.github.com>
2024-05-27 05:00:39 +0000

    Hi

--- app.py
+++ app.py
@@ -3,6 +3,5 @@ from PIL import Image
 st.set_page_config(page_title='The_Chicken_Empire', page_icon=":chicken:", layout= 'wide')
 with st.container():
     st.subheader("This is a test run")
-Im = Image.open('sub.png')
-st.image(Im)
-#python -m streamlit run app.py
+status = st.radio('Select gender:', ('Male', 'Female', 'Non-binary'))
+st.success(status)
