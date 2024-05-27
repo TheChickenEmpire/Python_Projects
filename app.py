e518197a248ef54b27c38c4338403179e7ec3019
TheChickenEmpire <164987492+TheChickenEmpire@users.noreply.github.com>
2024-05-27 06:15:08 +0000

    hi

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 from PIL import Image
 st.set_page_config(page_title='The_Chicken_Empire', page_icon=":chicken:", layout= 'wide')
-st.markdown("[![Click me](Stem.png)](https://teams.microsoft.com/v2/)")
+st.markdown("[![Click me](C:\Users\lucas\OneDrive\Pictures\Screenshots\Stem.png)](https://teams.microsoft.com/v2/)")
