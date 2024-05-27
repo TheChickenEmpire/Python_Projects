383c71482d17c5195154fc7dce3bad74d7989f66
TheChickenEmpire <164987492+TheChickenEmpire@users.noreply.github.com>
2024-05-27 09:32:56 +0000

    h

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ st.set_page_config(page_title='Stem Club', page_icon=":chicken:", layout= 'wide'
 st.header('Welcome')
 st.subheader(' to the stem club website')
 st.text("At STEM Club, we delve into the captivating world of coding and technological discoveries. \nOur mission is to cultivate a community of passionate individuals who are eager to unravel \nthe mysteries of science, technology, engineering, and mathematics. Whether you're a seasoned \nprogrammer or just starting your journey into the realm of technology, \nSTEM Club provides a vibrant and supportive environment where members can \ncollaborate, learn, and push the boundaries of their knowledge. \nFrom coding workshops to discussions on the latest breakthroughs in science and technology, \nthere's always something exciting happening at STEM Club.\nJoin us as we embark on a journey to unlock the endless possibilities of the digital age. \nTogether, let's inspire creativity, foster innovation, and shape the future through the power of STEM!")
-teams,  = st.columns([1])
+teams, im = st.columns([1])
 with teams:
     st.image(Image.open("Stem.png"), width= 600)
     st.markdown("https://teams.microsoft.com/v2/")
     st.link_button('Learn More', 'https://stemclublearn.streamlit.app/')
+with im:
+    st.image(Image.open("r.png"), width= 300)
