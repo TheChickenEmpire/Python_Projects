7e575950314e7fb5d1c79c0f710cdc88aa1ac417
TheChickenEmpire <164987492+TheChickenEmpire@users.noreply.github.com>
2024-05-27 04:51:59 +0000

    Hi

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+from PIL import Image
 st.set_page_config(page_title='The_Chicken_Empire', page_icon=":chicken:", layout= 'wide')
 with st.container():
     st.subheader("This is a test run")
+Im = Image.open('sub.png')
+st.image(Im)
 #python -m streamlit run app.py
