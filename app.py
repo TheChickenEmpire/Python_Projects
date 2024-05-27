ec7d300f06b6a81c4e639d1892f13f6c4eafb3b8
TheChickenEmpire <164987492+TheChickenEmpire@users.noreply.github.com>
2024-05-27 07:14:53 +0000

    Hi

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from PIL import Image
+st.header('Welcome')
+st.subheader(' to the stem club website')
 st.set_page_config(page_title='The_Chicken_Empire', page_icon=":chicken:", layout= 'wide')
 teams,  = st.columns([1])
 with teams:
