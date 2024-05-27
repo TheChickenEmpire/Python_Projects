ef211ce8b081f3d84b5784085803443eafc35f22
TheChickenEmpire <164987492+TheChickenEmpire@users.noreply.github.com>
2024-05-27 08:58:57 +0000

    hi

--- appLearn.py
+++ appLearn.py
@@ -6,4 +6,4 @@ st.header('Learn about stem club')
 st.text("Stem club is a place in lunch that you can code, learn, 3dmodel and more!!!\nIt is also free and gives you opportunities to learn to code for\nexample if you don't have time at home you can learn to make games\nand learn how to 3d model and we also have fun projects sometimes\nso if your interested come meet at N5 Matamoe Hub at Lunch time and bring your computer")
 st.image(Image.open('images.png'))
 if st.button('Back to main'):
-    keyboard.press_and_release('ctrl+w')
+    keyboard.press('ctrl+w')
