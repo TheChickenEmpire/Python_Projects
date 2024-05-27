6d8b25fc980e20a1d8648efc1e7188d31cebbd39
TheChickenEmpire <164987492+TheChickenEmpire@users.noreply.github.com>
2024-05-27 06:25:09 +0000

    hi

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import streamlit as st
 from PIL import Image
 st.set_page_config(page_title='The_Chicken_Empire', page_icon=":chicken:", layout= 'wide')
 st.image(Image.open("Stem.png"))
-st.page_link("https://teams.microsoft.com/v2/")
+st.page_link("https://teams.microsoft.com/v2/", 'For Teams Click Here')
