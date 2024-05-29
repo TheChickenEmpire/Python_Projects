2e8eb42d5c084c8030042e65b63908a4a191b592
TheChickenEmpire <164987492+TheChickenEmpire@users.noreply.github.com>
2024-05-29 19:06:35 +0000

    hi

--- app.py
+++ app.py
@@ -4,8 +4,20 @@ st.set_page_config(page_title='Chicken', page_icon=":chicken:", layout= 'wide')
 st.header('**Welcome to**')
 st.subheader(':red[**The Chicken Empire**]')
 st.subheader('_website_')
-st.text('')
-st.text('')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 st.subheader('Recent News:')
 st.text('Making Chicken Empire website')
 st.subheader('Wonderful portrait:')
