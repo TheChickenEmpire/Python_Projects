91e4d1854a4903c444188d2bc24a0d506fb2d553
TheChickenEmpire <164987492+TheChickenEmpire@users.noreply.github.com>
2024-05-29 18:56:58 +0000

    hi

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import streamlit as st
 from PIL import Image
 st.set_page_config(page_title='Chicken', page_icon=":chicken:", layout= 'wide')
-st.header('**Welcome to**')
+hi = st.columns(3)
+with hi:
+    st.header('**Welcome to**')
+with hi:
+    st.image(Image.open('Chicks.png'))
 st.subheader(':red[**The Chicken Empire**]')
 st.subheader('_website_')
 st.text('')
