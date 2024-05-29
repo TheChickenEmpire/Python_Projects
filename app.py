d950ae12ff832706789eb7043185faeca4a9eb42
TheChickenEmpire <164987492+TheChickenEmpire@users.noreply.github.com>
2024-05-29 10:00:11 +0000

    HI

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 from PIL import Image
 st.set_page_config(page_title='Chicken', page_icon=":chicken:", layout= 'wide')
-st.header('Welcome')
-st.subheader('to The Chicken Empire website')
+st.header('Welcome to')
+st.subheader('The Chicken Empire website')
