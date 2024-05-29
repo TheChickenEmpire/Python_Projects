454d29926fe4225fd6e60cb30eb7de1c679502d7
TheChickenEmpire <164987492+TheChickenEmpire@users.noreply.github.com>
2024-05-29 18:48:22 +0000

    hi

--- app.py
+++ app.py
@@ -3,4 +3,6 @@ from PIL import Image
 st.set_page_config(page_title='Chicken', page_icon=":chicken:", layout= 'wide')
 st.header('**Welcome to**')
 st.subheader(':red[**The Chicken Empire**]')
-st.subheader('_website_')
+st.subheader('_website_\n\n\n\n')
+st.subheader('Recent News:')
+st.text('Making Chicken Empire website')
