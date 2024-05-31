f11fa8e08211d6171ea3d152d142bed7a1cc9c7e
TheChickenEmpire <164987492+TheChickenEmpire@users.noreply.github.com>
2024-05-31 06:34:51 +0000

    HI

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 st.set_page_config(page_title='Chicken', page_icon=":chicken:", layout= 'wide')
 st.header('**Welcome to**')
-st.subheader(':red[**The Chicken Empire**]')
+st.subheader(':yellow[**The Chicken Empire**]')
 st.subheader('_website_')
 st.divider()
 st.subheader('Recent News:')
