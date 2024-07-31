8b213c3e04970abf5ced684518521ef71eed59c8
TheChickenEmpire <164987492+TheChickenEmpire@users.noreply.github.com>
2024-07-31 13:59:00 +1200

    Hi

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from PIL import Image
+import datetime as t
+if t.datetime.date() == '31/07/24':
+    st.snow()
 st.set_page_config(page_title='Chicken Empire', page_icon=":chicken:", layout= 'wide')
 st.logo(Image.open('Chicken.png'))
 st.header('Welcome to')
