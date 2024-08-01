90637f213270b153c189c7a556e3d0d3381c642d
TheChickenEmpire <164987492+TheChickenEmpire@users.noreply.github.com>
2024-08-01 12:03:06 +1200

    hi

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from PIL import Image
 st.set_page_config(page_title='Chicken Empire', page_icon=":chicken:", layout= 'wide')
 st.logo(Image.open('Chicken.png'))
 st.header('Welcome to')
-st.subheader(':rainbow[**The Chicken Empire**]')
+st.header(':rainbow[**The Chicken Empire**]')
 st.subheader('_website_') 
 st.divider()
 st.subheader('Recent News:')
