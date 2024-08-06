79a5cf8fa5a4581cbaf48f2322381802ee9ad713
TheChickenEmpire <164987492+TheChickenEmpire@users.noreply.github.com>
2024-08-06 22:39:31 +1200

    hi

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.header(':rainbow[**The Chicken Empire**]')
 st.subheader('_website_') 
 st.divider()
 currentDateAndTime = datetime.now()
-time = currentDateAndTime.strftime("%H:%M")
+time = str(currentDateAndTime.strftime("%H:%M"))
 if time < "12:00":
     time = "Good morning!"
 elif "12:00" <= time < "18:00":
