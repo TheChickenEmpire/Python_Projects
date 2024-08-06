c14a8d5695fabd674424546d54437c7edb265519
TheChickenEmpire <164987492+TheChickenEmpire@users.noreply.github.com>
2024-08-06 22:20:23 +1200

    Hi

--- app.py
+++ app.py
@@ -9,12 +9,6 @@ st.subheader('_website_')
 st.divider()
 currentDateAndTime = datetime.now()
 time = currentDateAndTime.strftime("%H:%M:%S")
-if time < "12:00":
-    time = "Good morning!"
-elif "12:00" <= time < "18:00":
-    time = "Good afternoon!"
-else:
-    time = "Good evening!"
 st.subheader(time)
 st.divider()
 st.subheader('Recent News:')
