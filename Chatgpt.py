331f85f0749e77ab2887a981a614cc2d79e0985b
TheChickenEmpire <164987492+TheChickenEmpire@users.noreply.github.com>
2024-08-06 21:46:22 +1200

    Hi

--- Chatgpt.py
+++ Chatgpt.py
@@ -1,12 +1,9 @@
 from openai import OpenAI
 
-client = OpenAI('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')
-
+client = OpenAI()
 stream = client.chat.completions.create(
     model="gpt-4o-mini",
     messages=[{"role": "user", "content": "Say this is a test"}],
     stream=True,
 )
-for chunk in stream:
-    if chunk.choices[0].delta.content is not None:
-        print(chunk.choices[0].delta.content, end="")
+print(stream)
