68134aaa542be2eb9dd4aa3ea2ef18de1a78b576
TheChickenEmpire <164987492+TheChickenEmpire@users.noreply.github.com>
2024-08-06 21:43:49 +1200

    hi

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from PIL import Image
+from datetime import datetime
 st.set_page_config(page_title='Chicken Empire', page_icon=":chicken:", layout= 'wide')
 st.logo(Image.open('Chicken.png'))
 st.subheader('Welcome to')
@@ -51,5 +52,51 @@ st.text('Credits: By Me')
 st.divider()
 st.subheader('Credits')
 st.text('Donnie(My other friend)\nZxJoshua33(You should go see his channel)\nOppositeAce8412(You should go see his channel)\nChickens\nEthan(My other friend)')
-if st.checkbox('Secret'):
-    st.link_button('Click here to \nredeem your prize','https://www.youtube.com/shorts/SXHMnicI6Pg')
+from vega_datasets import data
+
+from utils import chart, db
+
+COMMENT_TEMPLATE_MD = """{} - {}
+> {}"""
+
+
+def space(num_lines=1):
+    """Adds empty lines to the Streamlit app."""
+    for _ in range(num_lines):
+        st.write("")
+
+
+st.set_page_config(layout="centered", page_icon="💬", page_title="Commenting app")
+st.title("💬 Commenting app")
+source = data.stocks()
+all_symbols = source.symbol.unique()
+symbols = st.multiselect("Choose stocks to visualize", all_symbols, all_symbols[:3])
+space(1)
+source = source[source.symbol.isin(symbols)]
+chart = chart.get_chart(source)
+st.altair_chart(chart, use_container_width=True)
+space(2)
+conn = db.connect()
+comments = db.collect(conn)
+with st.expander("💬 Open comments"):
+    st.write("**Comments:**")
+    for index, entry in enumerate(comments.itertuples()):
+        st.markdown(COMMENT_TEMPLATE_MD.format(entry.name, entry.date, entry.comment))
+
+        is_last = index == len(comments) - 1
+        is_new = "just_posted" in st.session_state and is_last
+        if is_new:
+            st.success("☝️ Your comment was successfully posted.")
+    space(2)
+    st.write("**Add your own comment:**")
+    form = st.form("comment")
+    name = form.text_input("Name")
+    comment = form.text_area("Comment")
+    submit = form.form_submit_button("Add comment")
+
+    if submit:
+        date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        db.insert(conn, [[name, comment, date]])
+        if "just_posted" not in st.session_state:
+            st.session_state["just_posted"] = True
+        st.experimental_rerun()
