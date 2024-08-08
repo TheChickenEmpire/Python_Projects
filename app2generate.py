c58ab7b8072767f5f2604b5d4d4221b2fb095a52
TheChickenEmpire <164987492+TheChickenEmpire@users.noreply.github.com>
2024-08-08 21:50:58 +1200

    hi

--- app2generate.py
+++ app2generate.py
@@ -5,4 +5,4 @@ except:
 compete = 0
 while num > compete:
     compete=compete+1
-    print("st.image(Image.open('"+str(compete)+".png'), width=400)")
+    print("st.image(Image.open('"+str(compete)+".png'), width=500)")
