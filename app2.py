9dc10568c42240b3ba5dcb90b07aaa8889e5e667
TheChickenEmpire <164987492+TheChickenEmpire@users.noreply.github.com>
2024-08-08 13:00:13 +1200

    hi

---
+++ app2.py
@@ -0,0 +1,5 @@
+import streamlit as st
+from PIL import Image
+st.set_page_config(page_title='Chicken Empire Comic', page_icon=":chicken:", layout= 'wide')
+st.logo(Image.open('Chicken.png'))
+st.image(Image.open())
