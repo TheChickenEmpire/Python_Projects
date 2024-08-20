6d5a3c7053bbbd17db10af0c3e4f681d7f9aca50
TheChickenEmpire <164987492+TheChickenEmpire@users.noreply.github.com>
2024-08-21 11:30:22 +1200

    hoi

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def chat(asks: str, ans: str):
     line 2 is answers"""
     asks = asks.split(', ')
     ans = ans.split(', ')
-    ask = st.chat_input('You:\n').lower()
+    ask = st.chat_input('You:').lower()
     e = difflib.get_close_matches(ask, asks, 1)
     try:
         e = e[0]
