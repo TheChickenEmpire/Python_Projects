6422ece30bb75a71cc51f709c9234e2667b6b38a
TheChickenEmpire <164987492+TheChickenEmpire@users.noreply.github.com>
2024-08-21 20:54:22 +1200

    hi

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ with st.sidebar:
     st.subheader(':rainbow[Chicken Empire Comics]')
     st.link_button("Comics", 'https://thechickenempirecomic.streamlit.app/')
     st.markdown('Made by Oscar :red[**WARNING THERE IS \nA BIT OF BLOOD**]')
-    with st.chat_message('Chipbot'):
+    with st.chat_message('🍟'):
         st.info(chat('hi, hello, where can i find the comics, who is your favoirite youtuber, how do i get into the subscribers section on this website, how did you lose your channel, what is your favoirite food, Okay, what are you, are you happy, are you alive, English or spanish',
                      'hello, hi, up here ^^^^^^^^^^, The Chicken Empire you can check out his channel here>>>https://www.youtube.com/@TheChickenEmpire, you just need to sunscribe to the chicken empire, i accidently deleted because of google, mcdonalds, yeah, Im a assistant created by the website designer who also created The Chicken Empire, yes under circumstances, No well accordingly, Ancient greek, '))
 st.header('Welcome to')
