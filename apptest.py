f3ee6944759a88c41df1329ad8937cd03eae46cb
TheChickenEmpire <164987492+TheChickenEmpire@users.noreply.github.com>
2024-08-23 14:06:11 +1200

    hi

--- apptest.py
+++ apptest.py
@@ -27,6 +27,6 @@ def chat(asks: str, ans: str):
             return("Im sorry I don't understand what you mean")
 st.set_page_config('TEST', "🧪")
 st.header(':rainbow[_____-----Chipbot Assistant-----_____]')
-    with st.chat_message('🍟'):
-        st.info(chat('hi, hello, where can i find the comics, who is your favoirite youtuber, how do i get into the subscribers section on this website, how did you lose your channel, what is your favoirite food, Okay, what are you, are you happy, are you alive, English or spanish, what are you doing, whos the stupidest person in the world',
-                    'hello, hi, up here ^^^^^^^^^^, The Chicken Empire you can check out his channel here>>>https://www.youtube.com/@TheChickenEmpire, you just need to sunscribe to the chicken empire, i accidently deleted because of google, mcdonalds, yeah, Im a assistant created by the website designer who also created The Chicken Empire, yes under circumstances, No well accordingly, Ancient greek, nothing, Ethan'))
+with st.chat_message('🍟'):
+    st.info(chat('hi, hello, where can i find the comics, who is your favoirite youtuber, how do i get into the subscribers section on this website, how did you lose your channel, what is your favoirite food, Okay, what are you, are you happy, are you alive, English or spanish, what are you doing, whos the stupidest person in the world',
+                'hello, hi, up here ^^^^^^^^^^, The Chicken Empire you can check out his channel here>>>https://www.youtube.com/@TheChickenEmpire, you just need to sunscribe to the chicken empire, i accidently deleted because of google, mcdonalds, yeah, Im a assistant created by the website designer who also created The Chicken Empire, yes under circumstances, No well accordingly, Ancient greek, nothing, Ethan'))
