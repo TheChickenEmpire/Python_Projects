17f0f6116b0283b75c43e3c8bcdfe4a1e5a13575
TheChickenEmpire <164987492+TheChickenEmpire@users.noreply.github.com>
2024-08-23 14:03:32 +1200

    HI

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ st.logo(Image.open('Chicken.png'))
 with st.sidebar:
     st.header(':rainbow[_____-----Chipbot Assistant-----_____]')
     with st.chat_message('🍟'):
-        st.info(chat('hi, hello, where can i find the comics, who is your favoirite youtuber, how do i get into the subscribers section on this website, how did you lose your channel, what is your favoirite food, Okay, what are you, are you happy, are you alive, English or spanish, what are you doing, whos the worst idiot in the world',
+        st.info(chat('hi, hello, where can i find the comics, who is your favoirite youtuber, how do i get into the subscribers section on this website, how did you lose your channel, what is your favoirite food, Okay, what are you, are you happy, are you alive, English or spanish, what are you doing, whos the stupidest person in the world',
                     'hello, hi, up here ^^^^^^^^^^, The Chicken Empire you can check out his channel here>>>https://www.youtube.com/@TheChickenEmpire, you just need to sunscribe to the chicken empire, i accidently deleted because of google, mcdonalds, yeah, Im a assistant created by the website designer who also created The Chicken Empire, yes under circumstances, No well accordingly, Ancient greek, nothing, Ethan'))
     st.divider()
     st.subheader(':rainbow[My channel]')
