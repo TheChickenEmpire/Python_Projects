094e94e37bf88c45acafe5d819aeeaa19d371cc6
TheChickenEmpire <164987492+TheChickenEmpire@users.noreply.github.com>
2024-09-21 15:30:26 +1200

    Hi

--- apptest.py
+++ apptest.py
@@ -1,58 +1,73 @@
+import plotly.express as px
+import pandas as pd
 import streamlit as st
+import shelve
 
-from streamlit_server_state import server_state, server_state_lock
+from streamlit_plotly_events import plotly_events
 
-with server_state_lock["rooms"]:
-    if "rooms" not in server_state:
-        server_state["rooms"] = []
+DB_FILE = "db.shelve"
 
-rooms = server_state["rooms"]
+if "comments" not in st.session_state:
+    with shelve.open(DB_FILE) as db:
+        if "comments" in db:
+            st.session_state.comments = db["comments"]
+        else:
+            st.session_state.comments = {}
 
-room = st.sidebar.radio("Select room", rooms)
+if "selected_point" not in st.session_state:
+    st.session_state.selected_point = None
 
-with st.sidebar.form("New room"):
 
-    def on_create():
-        new_room_name = st.session_state.new_room_name
-        with server_state_lock["rooms"]:
-            server_state["rooms"] = server_state["rooms"] + [new_room_name]
+def point_to_key(point: dict[str, int]) -> str:
+    return f"{point['x']}_{point['y']}"
 
-    st.text_input("Room name", key="new_room_name")
-    st.form_submit_button("Create a new room", on_click=on_create)
 
-if not room:
+def key_to_xy(key: str) -> tuple[int, int]:
+    return tuple([int(a) for a in key.split("_")])
+
+
+points = pd.DataFrame({"x": [1, 2, 3, 4, 5, 6], "y": [1, 4, 9, 16, 25, 36]})
+points["has_comment"] = False
+
+for key, comment in st.session_state.comments.items():
+    x, y = key_to_xy(key)
+    points.loc[(points["x"] == x) & (points["y"] == y), "has_comment"] = True
+
+
+fig = px.scatter(
+    points,
+    x="x",
+    y="y",
+    color="has_comment",
+    color_discrete_map={True: "red", False: "blue"},
+)
+selected_points = plotly_events(fig)
+
+if selected_points:
+    st.session_state.selected_point = point_to_key(selected_points[0])
+
+if not st.session_state.selected_point:
     st.stop()
 
-room_key = f"room_{room}"
-with server_state_lock[room_key]:
-    if room_key not in server_state:
-        server_state[room_key] = []
 
-st.header(room)
-
-nickname = st.text_input("Nick name", key=f"nickname_{room}")
-if not nickname:
-    st.warning("Set your nick name.")
-    st.stop()
-
-message_input_key = f"message_input_{room}"
+def submit_comments(key: str):
+    if key not in st.session_state.comments:
+        st.session_state.comments[key] = []
+    st.session_state.comments[key].append(st.session_state["new_comment"])
+    with shelve.open(DB_FILE) as db:
+        db["comments"] = st.session_state.comments
 
 
-def on_message_input():
-    new_message_text = st.session_state[message_input_key]
-    if not new_message_text:
-        return
+key = st.session_state.selected_point
+if key in st.session_state.comments:
+    x, y = key_to_xy(key)
+    st.write(f"Comments: on point ({x}, {y}):")
+    for comment in st.session_state.comments[key]:
+        st.write(f"* {comment}")
 
-    new_message_packet = {
-        "nickname": nickname,
-        "text": new_message_text,
-    }
-
-    with server_state_lock[room_key]:
-        server_state[room_key] = server_state[room_key] + [new_message_packet]
-
-
-st.text_input("Message", key=message_input_key, on_change=on_message_input)
-
-st.subheader("Messages:")
-st.write(server_state[room_key])
+with st.form("comments"):
+    x, y = key_to_xy(key)
+    st.text_input(
+        f"What would you like to say about this point ({x}, {y})", key="new_comment"
+    )
+    st.form_submit_button("Submit", on_click=submit_comments, args=(key,))
